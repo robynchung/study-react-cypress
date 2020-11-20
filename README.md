7a774fd44655ac779fce3349a2a9cf50c6638937
Aeri <rjungdesign@gmail.com>
2020-11-19 19:26:42 -0500

    add cypress init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+https://docs.cypress.io/examples/examples/tutorials.html#1-Project-setup
+
 - npm install cypress -D
 - npx cypress open
---
+++ cypress/integration/input-form.spec.js
@@ -0,0 +1,5 @@
+describe("input form", () => {
+  it("focuses input on load", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
