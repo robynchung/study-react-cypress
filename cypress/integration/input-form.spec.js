f46fce43cf483809416b55d242efaa9b03cfa3fb
Aeri <rjungdesign@gmail.com>
2020-11-21 19:40:42 -0500

    add test cypress

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,17 @@
-describe("input form", () => {
-  it("focuses input on load", () => {
-    cy.visit("http://localhost:3000/");
+describe("Browswer actions", () => {
+  it("should load correct url", () => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("should check correct url ", () => {
+    cy.url().should("include", "localhost:3000");
+  });
+
+  it("should check for current element on the page", () => {
+    cy.get("form").should("be.visible");
+  });
+
+  it("should wait for 3 sec", () => {
+    cy.wait(3000);
   });
 });
